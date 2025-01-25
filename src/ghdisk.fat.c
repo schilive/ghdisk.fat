25d559f150f5759a32e1d3d7d6df87d030897607
schilive <schilive100@gmail.com>
2025-01-25 01:46:09 -0300

    feat: basic ad-hoc command-line parsing with no command
    The current implementation is meant to be temporary, and grow into its
    own file.
    
    If nothing is given, the usage message is printed with success.
    Otherwise, the syntax is "<options> <command> [...]", which restricts
    that commands cannot start with "-".
    
    The options are help options ("--help" or "-h") or version options
    ("--version" or "-v"). None of them take an argument. If only the
    version option is set, then the version message is printed with success;
    if only the help option is set, then the usage message is printed with
    success. If both are set, the help message takes preference.
    
    Short option syntax is implemented, such that each letter is one option,
    and the next is either its argument or the next option. For example, the
    argument "-vah" is equivalent to "-v -a -h".
    
    The equal sign for separating option name and argument is not implemented, yet..

--- src/ghdisk.fat.c
+++ src/ghdisk.fat.c
@@ -25,9 +25,98 @@ SOFTWARE.
 /* This is the entry function. */
 
 #include <stdio.h>
+#include <string.h>
+
+static void usage(void)
+{
+        printf(
+                "Usage: [ops...] <command> <command args...>\n"
+                "\n"
+                "Options:\n"
+                "        --help, -h      Display help message.\n"
+                "        --version, -v   Displays version.\n"
+        );
+}
+
+static void version(void)
+{
+        printf("Version 0.0.0\n");
+}
 
 int main(int argc, char *argv[])
 {
-        printf("Hello, World!\n");
-        return 0;
+        int i;
+        int h = 0, v = 0;
+        int cmd = -1;
+        if (argv[0] == NULL || argv[1] == NULL) {
+                usage();
+                return 0;
+        }
+
+        for (i = 1; argv[i] != NULL; i++) {
+                const char *arg = argv[i];
+                int opt_long = 0;
+                if (arg[0] != '-' || arg[1] == 0) {
+                        cmd = i;
+                        break;
+                }
+                opt_long = arg[1] == '-';
+
+                if (opt_long) {
+                        if (strcmp(arg, "--help") == 0) {
+                                h = 1;
+                                continue;
+                        }
+                        if (strcmp(arg, "--version") == 0) {
+                                v = 1;
+                                continue;
+                        }
+                }
+                else {
+                        int j;
+                        for (j = 1; arg[j] != 0; j++) {
+                                if (arg[j] == 'h') {
+                                        h = 1;
+                                        continue;
+                                }
+                                if (arg[j] == 'v') {
+                                        v = 1;
+                                        continue;
+                                }
+                                break;
+                        }
+                }
+
+                if (h || v)
+                        break;
+
+                (void)fprintf(stderr, "Fatal error: unknown option given: '");
+                if (opt_long)
+                        (void)fprintf(stderr, "%s", arg);
+                else
+                        (void)fprintf(stderr, "-%c", arg[1]);
+                (void)fprintf(stderr, "'\n");
+                return 1;
+        }
+
+        if (h) {
+                usage();
+                return 0;
+        }
+        if (v) {
+                version();
+                return 0;
+        }
+
+        if (cmd == -1)
+                goto error_no_command;
+        (void)fprintf(stderr,
+                        "Fatal error: unknown command given: '%s'\n", 
+                        argv[i]
+                     );
+        return 1;
+
+error_no_command:
+        (void)fprintf(stderr, "Fatal error: no command given\n");
+        return 1;
 }
