9d85e098d677860f3eadd217a4c97857b613fad9
schilive <schilive100@gmail.com>
2025-02-01 19:38:54 -0300

    feat: command 'replace_c_file'

--- lang/build.py
+++ lang/build.py
@@ -3,10 +3,11 @@ import re
 
 
 G_KEYWORD = '_'
-# The following regexes/regeces/regrets have each one capturing group
+# The following regexes/regeces/regrets have each one or more capturing groups
 R_STR = r'(?:(?<!\\)"' + r'((?:(?!\\|").|\\.|\s)*)' + r'")'             # Captures the string content
 R_STRS = r'((?:' + R_STR + r'\s*)+)'                                    # " the series of strings
-R_CSTRS = rf'\b{G_KEYWORD}\b\s*(?:\(+)\s*{R_STRS}\s*(?:\)+)\s*(?=[,)])'   # " " " " " as the 1st parm in the func keyword
+R_CSTRS = rf'\b{G_KEYWORD}\b\s*(?:\(+)\s*{R_STRS}\s*(?:\)+)\s*(?=[,)])' # " " " " " as the 1st parm in the func keyword
+R_MSG = r'^\s*msgid\s*' + R_STR + r'\s*^\s*msgstr\s*' + R_STR + r'\s*'   # " the msgid and msgstr strings
 
 
 class CString:
@@ -36,13 +37,50 @@ class CStrings:
         else:
             self.strings[c.string] = [c]
 
-    def __init__(self, cs: list[CString] = None):
+    def __init__(self, cs: list[CString] | None = None):
         if cs is None:
             cs = []
         self.list = []
         self.strings = dict({})
         for c in cs:
             self.add(c)
+
+
+class PotString:
+    def set_msgid(self, msgid: str):
+        assert(type(msgid) == str)
+        self.msgid = msgid
+
+    def set_msgstr(self, msgstr: str):
+        assert(type(msgstr) == str)
+        self.msgstr = msgstr
+
+    def set_location(self, location: list[int]):
+        assert(type(location) == list)
+        assert(len(location) == 2)
+        for i in location:
+            assert(type(i) == int)
+        location[1] >= location[0]
+        self.location = location
+
+    def __init__(self, msgid: str, msgstr: str, location: list[int]):
+        self.set_msgid(msgid)
+        self.set_msgstr(msgstr)
+        self.set_location(location)
+
+
+class PotStrings:
+    def add(self, p: PotString):
+        assert(type(p) == PotString)
+        assert(p.msgid not in self.dict)
+        self.dict[p.msgid] = p
+
+    def __init__(self, ps: list[PotString] | None = None):
+        if ps is None:
+            ps = []
+        self.dict = dict({})
+        for p in ps:
+            self.add(p)
     
 
 def parse_c_file(content: str) -> CStrings:
@@ -58,6 +96,19 @@ def parse_c_file(content: str) -> CStrings:
             string += m_str
         #print(f'STR = {string} = {content[start:end]}')
         result.add(CString(string, [start, end]))
+    return result
+
+
+def parse_pot(content: str) -> PotStrings:
+    assert(type(content) == str)
+
+    result: PotStrings = PotStrings()
+    for m in re.finditer(R_MSG, content, re.M):
+        msgid = m.groups()[0]
+        msgstr = m.groups()[1]
+        location = [m.start(), m.end()]
+        #print(f'MSG = ({msgid}, {msgstr}, {location})')
+        result.add(PotString(msgid, msgstr, location))
     return result
 
 
@@ -87,26 +138,65 @@ def cmd_make_pot(c_filepath: str, pot_filepath: str):
     pot_file.close()
 
 
+def cmd_replace_c_file(pot_filepath: str, c_filepath: str):
+    assert(type(c_filepath) == str)
+    assert(type(pot_filepath) == str)
+
+    pot_file = open(pot_filepath,  'rt')
+    pot_content = pot_file.read()
+    pot_file.close()
+
+    pot_ps = parse_pot(pot_content)
+    del pot_content
+    print(pot_ps.__dict__)
+
+    c_file = open(c_filepath, 'rt')
+    c_content = c_file.read()
+    c_file.close()
+
+    c_cs = parse_c_file(c_content)
+    # We need to sort otherwise the location may become incorrect.
+    #
+    # For example, if we have the file 'ab cd' and the locations [[0, 1], [3, 4]], then if we first substitute the first
+    # location to 'pt_ab', we will have 'pt_ab cd'. Then if we try to substitute the second location to 'pt_cd', we will
+    # have 'pt_pt_cd'.
+    c_cs.list.sort(key=lambda x: x.location[0], reverse=True)
+    for c in c_cs.list:
+        if c.string not in pot_ps.dict:
+            continue
+        p = pot_ps.dict[c.string]
+        c_content = c_content[:c.location[0]] + '"' + p.msgstr + '"' + c_content[c.location[1]:]
+
+    c_file = open(c_filepath, 'wt')
+    c_file.write(c_content)
+    c_file.close()
+
+
 def main():
     if len(sys.argv) <= 1:
         print('Usage: <command> [<args...>]')
         print('Commands:')
         print('\tmake_pot <c_file> <pot_file>')
+        print('\treplace_c_file <pot_file> <c_file>')
         print('')
         sys.exit(0)
     command = sys.argv[1]
     command_argv = sys.argv[2:]
+    if len(command_argv) < 2:
+        print('Fatal error: required arguments not given', file=sys.stderr)
+        sys.exit(1)
+    elif len(command_argv) > 2:
+        print('Fatal error: too many argument given', file=sys.stderr)
+        sys.exit(1)
 
     if command == 'make_pot':
-        if len(command_argv) < 2:
-            print('Fatal error: required arguments not given', file=sys.stderr)
-            sys.exit(1)
-        elif len(command_argv) > 2:
-            print('Fatal error: too many argument given', file=sys.stderr)
-            sys.exit(1)
         c_file = command_argv[0]
         pot_file = command_argv[1]
         cmd_make_pot(c_file, pot_file)
+    elif command == 'replace_c_file':
+        pot_file = command_argv[0]
+        c_file = command_argv[1]
+        cmd_replace_c_file(pot_file, c_file)
     else:
         print(f'Fatal error: unknown command: \'{command}\'', file=sys.stderr)
         sys.exit(1)
