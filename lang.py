6fbed5a00dca23a4cc6ca25a5f265d00a0c7cf9d
schilive <schilive100@gmail.com>
2025-06-14 00:48:22 -0300

    build: add 'apply_po' command for 'lang.py'
    The command 'apply_po <po> <c file> <out>' parses '<po>' to find all
    translations, and replaces all instances of the keyword 'STR_TRN()' and
    its parameter to their translation, in '<c file>'. However, so it does
    not modify '<c file>', it outputs the replacements result to '<out>'.

--- lang.py
+++ lang.py
@@ -3,22 +3,47 @@
 import sys
 
 G_C_SPACE_CHARACTERS = set([" ", "\t", "\v", "\n"])
+G_PO_SPACE_CHARACTERS = set([" ", "\t", "\v"])
 
 # Implementation-Specific
 G_FILE_ENCODING = "ascii"
 G_KEYWORD = "STR_TRN"
 
 
+class CId:
+    def __init__(self, content, begin, end):
+        self.content = content
+        self.begin = begin
+        self.end = end
+
+
+def replace_all(target, indeces, replacements):
+    while len(indeces) != 0:
+        furthest_indeces_index = 0
+        for i in range(1, len(indeces)):
+            if indeces[i][0] > furthest_indeces_index[0]:
+                furthest_indeces_index = i
+        begin_index = indeces[furthest_indeces_index][0]
+        end_index = indeces[furthest_indeces_index][1]
+        replacement = "\"" + replacements[furthest_indeces_index] + "\""
+
+        target = target[:begin_index] + replacement + target[end_index + 1:]
+
+        indeces.pop(furthest_indeces_index)
+        replacements.pop(furthest_indeces_index)
+    return target
+
+
 def print_fatal(message):
     print("Fatal error: " + message, file=sys.stderr)
 
 
-# Returns a set (literally type "set") with all the translation identifiers
+# Returns a list of 'CId's with all translation identifiers and their begining and ending indeces.
 def parse_c(file):
     # <keyword> <space*>
 
     offset = 0
-    result = set([])
+    result = []
     while True:
         begin = file.find(G_KEYWORD, offset)
         if begin == -1:
@@ -44,14 +69,71 @@ def parse_c(file):
             string += file[offset]
             offset += 1
 
+        #assert(file[offset] == "\"")
+        offset += 1
+
         while file[offset] in G_C_SPACE_CHARACTERS:
             offset += 1
 
         #assert(file[offset] == ")")
         offset += 1
 
-        result = result.union(set([string]))
+        result.append(CId(string, begin, offset - 1))
         continue
+    return result
+
+
+# Returns dict[str, str]
+def parse_po(file):
+    result = dict([])
+    lines = file.split("\n")
+    msgid = None
+    for l in lines:
+        offset = 0
+        while offset < len(l) and l[offset] in G_PO_SPACE_CHARACTERS:
+            offset += 1
+        if offset == len(l):
+            continue
+        # Else it is a <command>
+        
+        while l[offset] in G_PO_SPACE_CHARACTERS:
+            offset += 1
+
+        name = ""
+        while l[offset] not in G_PO_SPACE_CHARACTERS:
+            name += l[offset]
+            offset += 1
+        
+        while l[offset] in G_PO_SPACE_CHARACTERS:
+            offset += 1
+
+        #assert(l[offset] == "\"")
+        offset += 1
+
+        parameter = ""
+        while True:
+            if l[offset] == "\"":
+                break
+            if l[offset] == "\\" and l[offset] == "\"":
+                offset += 1
+            parameter += l[offset]
+            offset += 1
+        
+        #assert(l[offset] == "\"")
+        offset += 1
+
+        while offset < len(l) and l[offset] in G_PO_SPACE_CHARACTERS:
+            offset += 1
+
+        if offset < len(l):
+            #assert(l[offset] = "\n")
+            offset += 1
+
+        if name == "msgid":
+            msgid = parameter
+            continue
+        #assert(name == "msgstr")
+        result[msgid] = parameter
     return result
 
 
@@ -80,7 +162,10 @@ def cmd_create_pot(argv):
     file_in_content = file_in_object.read()
     file_in_object.close()
 
-    ids = parse_c(file_in_content)
+    cids = parse_c(file_in_content)
+    ids = []
+    for i in cids:
+        ids.append(i.content)
 
     file_out_object = open(
         argv[2], 
@@ -106,6 +191,53 @@ def cmd_create_po(argv):
     return 0
 
 
+def cmd_apply_po(argv):
+    if len(argv) < 4:
+        print_fatal("Not enough arguments given")
+        return 1
+    file_po_name = argv[1]
+    file_c_name = argv[2]
+    file_out_name = argv[3]
+
+    file_po_object = open(
+        file_po_name, 
+        mode="rt", 
+        encoding=G_FILE_ENCODING,
+        errors="strict"
+    )
+    file_po_content = file_po_object.read()
+    file_po_object.close()
+    file_c_object = open(
+        file_c_name, 
+        mode="rt", 
+        encoding=G_FILE_ENCODING,
+        errors="strict"
+    )
+    file_c_content = file_c_object.read()
+    file_c_object.close()
+
+    cids = parse_c(file_c_content)
+    trans = parse_po(file_po_content)
+
+    replace_indeces = []
+    replace_replacements = []
+    for i in cids:
+        replace_indeces += [[i.begin, i.end]]
+        replace_replacements.append(trans[i.content])
+    file_out_content = replace_all(file_c_content, replace_indeces, replace_replacements)
+
+    file_out_object = open(
+        file_out_name, 
+        mode="wt", 
+        encoding=G_FILE_ENCODING,
+        errors="strict"
+    )
+    file_out_object.write(file_out_content)
+    file_out_object.close()
+
+    return 0
+
+
 def main(argv):
     command = None
     if len(argv) < 2:
@@ -117,6 +249,8 @@ def main(argv):
         return cmd_create_pot(argv[1:])
     elif command == "create_po":
         return cmd_create_po(argv[1:])
+    elif command == "apply_po":
+        return cmd_apply_po(argv[1:])
     print_fatal("Unknown command given: '" + command + "'")
     return 1
 
